ae9ff72420d21aeb9997cef1a7ea3f01324247d7
Mike Love <mikelove1@gmail.com>
2015-02-19 09:56:11 -0500

    adding knit script

---
+++ knit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+############################################
+#
+# usage:
+#
+# ./knit.sh course1 random_variables
+#
+############################################
+
+printf "\n  *** knit *** \n\n"
+
+cd ../labs/$1
+Rscript -e "library(knitr); knit('$2.Rmd')"
+
+printf "\n  *** done! *** \n\n"
